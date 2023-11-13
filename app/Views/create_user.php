9269a6c82164aba0d7509782aa621a100e1c74f0
Sebelum <myshel.wihasna21@students.unila.ac.id>
2023-11-14 06:34:13 +0700

    perbaikan_tampilan_user

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -1,31 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
     <title>Create User</title>
+    
 </head>
 <body>
-    <h2>Create User</h2>
+    <div class="container" style="padding:20px ">
+        <div class="row my-3">
+            <div class="col-md">
+                <h2><i class="bi bi-person-plus"></i>&nbsp;user</h2>
+                <hr>
+            </div>
+        </div>
 
     <form action="<?= base_url('/user/store') ?>" method="POST" enctype="multipart/form-data">
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required><br>
+       
+        <div class="mb-3">
+            <label for="username" class="form-label">Username :</label>
+            <input type="text" id="username" name="username" class="form-control w-50" >
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password :</label>
+            <input type="password" id="password" name="password" class="form-control w-50" required aria-describedby="passwordHelpBlock">
+        </div>
+      
+        <div class="mb-3">
+            <label for="nama_user" class="form-label">Nama User: :</label>
+            <input type="text" id="nama_user" name="nama_user" class="form-control w-50" >
+        </div>
 
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required><br>
-
-        <label for="nama_user">Nama User:</label>
-        <input type="text" name="nama_user" id="nama_user" required><br>
-
-        <label for="no_ktp">Nomor KTP:</label>
-        <input type="text" name="no_ktp" id="no_ktp" required><br>
+        <div class="mb-3">
+            <label for="no_ktp" class="form-label">Nomor KTP: :</label>
+            <input type="text" id="no_ktp" name="no_ktp" class="form-control w-50" >
+        </div>
         
-        <label for="no_tlp">Nomor Telepon:</label>
-        <input type="text" name="no_tlp" id="no_tlp" required><br>
+        <div class="mb-3">
+            <label for="username" class="form-label">Nomor Telepon: :</label>
+            <input type="text" id="no_tlp" name="no_tlp" class="form-control w-50" >
+        </div>
 
-        <label for="alamat">Alamat:</label>
-        <textarea name="alamat" id="alamat" required></textarea><br>
+        <div class="mb-3">
+            <label for="alamat" class="form-label">Alamat :</label>
+            <textarea class="form-control-w-50" id="alamat" name="alamat" rows="3"></textarea>
+        </div>
 
-        <input type="submit" value="Submit">
+        <button type="submit" class="btn btn-primary">
+            <i class="bi bi-send"></i> Submit
+        </button>
     </form>
+    </div>
 </body>
 </html>
